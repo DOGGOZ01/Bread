130cbfcfd52d62fc11ea8f75e150b2b9eb630d98
Zyksa <deveaudt@gmail.com>
2021-06-03 03:23:40 +0200

    edit embed

--- commands/admin/start.js
+++ commands/admin/start.js
@@ -7,11 +7,8 @@ module.exports = {
 
     run: async (message, args, client, Discord) => {
 
-        message.channel.send(":bellhop: Si vous avez besoin d'aide, ouvrez un ticket en cliquant sur la réaction :envelope_with_arrow:")
-
-
         const embed = new Discord.MessageEmbed()
-            .setTitle("Créer un nouveau ticket")
+            .setTitle(":bellhop: Créer un nouveau ticket")
             .setColor('#5e00ff')
             .setDescription("En réagissant ci-dessous vous pouvez créer un nouveau ticket d'aide")
 
