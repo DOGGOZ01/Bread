3c6f4f11ada54b27d323553e4e7be5fed63a45ec
Hokanosekai <69720899+Hokanosekai@users.noreply.github.com>
2021-06-03 20:10:23 +0200

    create tickets

--- commands/admin/start.js
+++ commands/admin/start.js
@@ -6,15 +6,29 @@ module.exports = {
     aliases: null,
 
     run: async (message, args, client, Discord) => {
+        const exec = message.member
 
         const embed = new Discord.MessageEmbed()
             .setTitle(":bellhop: Ouvrir une demande d'aide")
             .setColor('#5e00ff')
-            .setDescription("Cliquez sur la réaction pour créer un channel dédié à votre problème.")
+            .setDescription("Cliquez sur une réaction pour créer un channel dédié à votre problème.")
 
-        message.channel.send(embed).then(e => {
-            e.react('📩');
-        });
+        const message_send = await message.channel.send(embed);
 
+        const language_reactions = [
+            '849947825173037056',
+            '849947825068048444',
+            '849947825117593620',
+            '849947825026629693',
+            '849947825227300874',
+            '849947825105535036',
+            '849947825043013642',
+            '849947825043537940',
+            '849959938880962570'
+        ];
+
+        for (const lr of language_reactions) {
+            await message_send.react(lr);
+        }
     }
 }
--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -7,55 +7,65 @@ module.exports = {
         const reactions = message.reactions;
         const channel_id = reaction.message.channel.id;
 
-        const everyone = message.guild.roles.cache.get(process.env.CAT_OPEN);
+        const everyone = message.guild.roles.cache.get(process.env.EVERYONE);
 
         if (!reaction.partial) return;
 
 
-        const language_reactions = [
-            '849947825173037056',
-            '849947825068048444',
-            '849947825117593620',
-            '849947825026629693',
-            '849947825227300874',
-            '849947825105535036',
-            '849947825043013642',
-            '849947825043537940',
-            '🅰️'
-        ];
-
-
         if (channel_id === process.env.CHANNEL_START) {
-            /* Remove Automatically User Reactions */
+            console.log("test")
             await reaction.users.remove(user);
-            message.guild.channels.create(`ticket ${user.username}`, {type: 'text'}).then(async channel => {
-                let category = message.guild.channels.cache.get('849823383507697734');
-                await channel.setParent(category);
 
-                channel.updateOverwrite(everyone, {
-                    SEND_MESSAGES: true,
-                    VIEW_CHANNEL: true
-                }).then(async channel_t => {
+            const language_reactions = [
+                '849947825173037056',
+                '849947825068048444',
+                '849947825117593620',
+                '849947825026629693',
+                '849947825227300874',
+                '849947825105535036',
+                '849947825043013642',
+                '849947825043537940',
+                '849959938880962570'
+            ];
 
-                    const embed = new Discord.MessageEmbed()
-                        .setTitle("Ticket de demande d'aide")
-                        .setColor('#5e00ff')
-                        .setDescription(`<@${user.id}> Tu peux poser ta question ou exposer ton problème ici.\n` +
-                            "Les autres pourront ainsi t'aider le résoudre\n\n" +
-                            "Tu peux commencer par sélectionner le langage :")
+            await reactions.cache.forEach((item, key, obj) => {
+                if (!language_reactions.includes(key)) return
 
-                    const message_send = await channel_t.send(embed);
-                    for (const lr of language_reactions) {
-                        console.log(lr)
-                        await message_send.react(lr);
-                    }
+                const language = item.emoji.name !== 'a_'? item.emoji.name : 'autre';
+
+                message.guild.channels.create(`${language}-${user.username}`, {type: 'text'}).then(async channel => {
+                    let category = message.guild.channels.cache.get(process.env.CAT_OPEN);
+                    await channel.setParent(category);
+
+                    await channel.updateOverwrite(everyone, {
+                        SEND_MESSAGES: true,
+                        VIEW_CHANNEL: true
+                    }).then(async channel_t => {
+
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle(`Demande d'aide en ${language}`)
+                            .setColor('#5e00ff')
+                            .setDescription(`<@${user.id}> Tu peux poser ta question ou exposer ton problème ici.\n` +
+                                "Les autres pourront ainsi t'aider le résoudre\n\n" +
+                                "Tu peux commencer par sélectionner le langage :")
+
+                        await channel_t.send(embed);
+                        await obj.delete(key);
+                    });
                 });
-            });
+
+                const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
+                try {
+                    console.log(userReactions.size)
+                    for (const reaction of userReactions.values()) {
+                        reaction.users.remove(user.id);
+                    }
+                    console.log(userReactions.size)
+                } catch (error) {
+                    console.error('Failed to remove reactions.');
+                }
+            })
+
         }
-
-
-
-
-
     }
 }
