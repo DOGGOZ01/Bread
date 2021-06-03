05f8bd799474c6d708643e33035c766c2cb1d244
Zyksa <deveaudt@gmail.com>
2021-06-03 02:16:35 +0200

    modify activity status

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,17 +5,19 @@ module.exports = {
     execute: async (client, Discord) => {
         console.log('>>> Bread Bot is Online <<<');
 
-        let i = 0;
-        setInterval(() => {
-            const status = [
-                'dev with :heart:',
-                'im helping you',
-                'to find dev solution'
-            ];
+        client.user.setActivity("attendre vos demandes d'aides...")
 
-            client.user.setActivity(status[i]).then(() => {
-                i = ++i % status.length;
-            });
-        }, 2000);
+        // let i = 0;
+        // setInterval(() => {
+        //     const status = [
+        //         'dev with :heart:',
+        //         'im helping you',
+        //         'to find dev solution'
+        //     ];
+        //
+        //     client.user.setActivity(status[i]).then(() => {
+        //         i = ++i % status.length;
+        //     });
+        // }, 2000);
     }
 }
