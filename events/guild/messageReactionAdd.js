1373bfeaf9e2e68e19012565b3a028fc812d2879
Hokanosekai <69720899+Hokanosekai@users.noreply.github.com>
2021-06-03 12:30:28 +0200

    t

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -7,40 +7,54 @@ module.exports = {
         const reactions = message.reactions;
         const channel_id = reaction.message.channel.id;
 
-        const everyone = message.guild.roles.cache.get('744192330344431797');
+        const everyone = message.guild.roles.cache.get(process.env.CAT_OPEN);
 
         if (!reaction.partial) return;
 
 
-        /* Remove Automatically User Reactions */
-        await reaction.users.remove(user);
+        const language_reactions = [
+            '849947825173037056',
+            '849947825068048444',
+            '849947825117593620',
+            '849947825026629693',
+            '849947825227300874',
+            '849947825105535036',
+            '849947825043013642',
+            '849947825043537940',
+            '🅰️'
+        ];
 
 
-        if (channel_id === '849817571456843787') {
-            message.guild.channels.create(`ticket ${user.username}`, {type: 'text'}).then(channel => {
+        if (channel_id === process.env.CHANNEL_START) {
+            /* Remove Automatically User Reactions */
+            await reaction.users.remove(user);
+            message.guild.channels.create(`ticket ${user.username}`, {type: 'text'}).then(async channel => {
                 let category = message.guild.channels.cache.get('849823383507697734');
-                channel.setParent(category);
+                await channel.setParent(category);
 
                 channel.updateOverwrite(everyone, {
                     SEND_MESSAGES: true,
                     VIEW_CHANNEL: true
                 }).then(async channel_t => {
+
                     const embed = new Discord.MessageEmbed()
                         .setTitle("Ticket de demande d'aide")
                         .setColor('#5e00ff')
                         .setDescription(`<@${user.id}> Tu peux poser ta question ou exposer ton problème ici.\n` +
-                            "Les autres pourront ainsi t'aider le résoudre\n" +
-                            "Pour une meilleur comprehension du sujet merci de respecter les règles suivantes :")
-                        .addFields({name: "Définis le language", value: "JAVA, Python, etc, ..."},
-                            {name: "Enonce clairement ton problème", value: "Je ne comprend pas comment faire une recherche Google ?"},
-                            {name: "Ajoute du code/image", value: "\`from maths import *\`"},
-                            {name: "Merci de fermé le ticket", value: "Réagit sur la réaction ❌"})
+                            "Les autres pourront ainsi t'aider le résoudre\n\n" +
+                            "Tu peux commencer par sélectionner le langage :")
 
-                    const t = await channel_t.send(embed);
-                    t.react('❌');
-                })
+                    const message_send = await channel_t.send(embed);
+                    for (const lr of language_reactions) {
+                        console.log(lr)
+                        await message_send.react(lr);
+                    }
+                });
             });
         }
+
+
+
 
 
     }
