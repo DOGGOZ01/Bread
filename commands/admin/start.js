f0cb3d63d4c1ffecdc8f02d6faafe62f8ce9282b
Zyksa <deveaudt@gmail.com>
2021-06-03 03:19:38 +0200

    add msg on command start

--- commands/admin/start.js
+++ commands/admin/start.js
@@ -7,6 +7,9 @@ module.exports = {
 
     run: async (message, args, client, Discord) => {
 
+        message.channel.send(":bellhop: Si vous avez besoin d'aide, ouvrez un ticket en cliquant sur la réaction :envelope_with_arrow:")
+
+
         const embed = new Discord.MessageEmbed()
             .setTitle()
             .setColor()
