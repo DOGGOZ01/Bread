abb218c6693ac9bd94a27e971dda7cde145d7371
Zyksa <deveaudt@gmail.com>
2021-06-03 03:27:34 +0200

    create clear js

---
+++ commands/user/clear.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'clear',
+    args: true,
+    admin: false,
+    loaded: true,
+    aliases: null,
+
+    run: async (message, args, client, Discord) => {
+
+    }
+}
