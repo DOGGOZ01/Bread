079cf2dcf287b9f25373ae80c9f5547dee6e04c2
Zyksa <deveaudt@gmail.com>
2021-06-03 03:36:50 +0200

    update clear command

--- commands/user/clear.js
+++ commands/user/clear.js
@@ -6,6 +6,26 @@ module.exports = {
     aliases: null,
 
     run: async (message, args, client, Discord) => {
+        const exec = message.member
 
+        message.delete()
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.reply("Tu n'as pas la permission de faire ça");
+        }
+
+        const reason = args.slice(0).join(' ')
+        if (!reason) {
+            message.reply("Indique le nombre de messages que tu souhaite supprimer.");
+        }
+
+        const deleteCount = parseInt(reason, 10);
+
+        if (!deleteCount || deleteCount < 1 || deleteCount > 100) return message.reply("Tu peux pas supp + de 99 msg en 1 fois.");
+
+        message.channel.bulkDelete(deleteCount + 1).catch(error => message.reply(`Erreur: ${error}`));
+
+
+        message.channel.send(exec + " vient de supprimer: " + reason + " messages");
     }
 }
